156816835abd0cbc8d05f6b6f72d9feaa1044564
jerzakm <jerzakm@gmail.com>
2023-09-11 11:06:37 +0200

    starlight docs

---
+++ apps/docs/astro.config.mjs
@@ -0,0 +1,48 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+
+const noExternal = ['three', 'troika-three-text', 'postprocessing']
+if (process.env.NODE_ENV === 'production') {
+  noExternal.push('@theatre/core')
+}
+
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'threejs-kit - materials, shaders and utils for three.js',
+      social: {
+        github: 'https://github.com/jerzakm/threejs-kit',
+      },
+      sidebar: [
+        {
+          label: 'Intro',
+          autogenerate: { directory: 'intro' },
+        },
+        {
+          label: 'Materials',
+          autogenerate: { directory: 'materials' },
+        },
+      ],
+      customCss: ['./src/tailwind.css'],
+
+    }),
+    tailwind({ applyBaseStyles: false }),
+    svelte()
+  ],
+  vite: {
+    ssr: {
+      // "@theatre/core" needs to be externalized in development mode but not in production!
+      noExternal: noExternal
+    },
+    resolve: {
+      alias: {
+        'lib/*': './src/lib'
+      }
+    }
+  },
+  output: 'static'
+});
