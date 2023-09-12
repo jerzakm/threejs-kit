2689158664a5826035028d07d971f5bb994c8d75
Marcin Jerzak <jerzakm@gmail.com>
2023-09-12 15:44:18 +0200

    - first info about parallax docs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'production') {
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'threejs-kit - materials, shaders and utils for three.js',
+      title: 'threejs-kit',
       social: {
         github: 'https://github.com/jerzakm/threejs-kit',
       },
