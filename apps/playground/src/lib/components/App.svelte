13a02e74320eae1f46ed8dad187a3b5b5fd5cfdf
Marcin Jerzak <jerzakm@gmail.com>
2024-02-29 11:19:16 +0100

    pg fix

--- apps/playground/src/lib/components/App.svelte
+++ apps/playground/src/lib/components/App.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import { Canvas } from '@threlte/core';
 	import Renderer from './Renderer.svelte';
-
- 
 </script>
 
-<Canvas>
-	<Renderer />
-	<slot />
-</Canvas>
+<div>
+	<Canvas>
+		<Renderer />
+		<slot />
+	</Canvas>
+</div>
+
+<style>
+	div {
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+</style>
