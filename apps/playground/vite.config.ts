1cf9b2cc04eddc2656d917d46ddadc895cdcdcbb
Marcin Jerzak <jerzakm@gmail.com>
2024-03-07 11:23:27 +0100

    ww head

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,20 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const viteHeaderPlugin = {
+	name: 'add headers',
+	configureServer: (server) => {
+		server.middlewares.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET');
+			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+			next();
+		});
+	}
+};
+
 export default defineConfig({
-	plugins: [
-		{
-			name: 'configure-response-headers',
-			configureServer: (server) => {
-				server.middlewares.use((_req, res, next) => {
-					res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-					res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-					next();
-				});
-			}
-		},
-		sveltekit()
-	],
+	plugins: [viteHeaderPlugin, sveltekit()],
 	ssr: {
 		noExternal: ['three']
 	},
